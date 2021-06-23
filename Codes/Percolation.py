bee336e50f43d8e621e75a2afb5c7c4f6cba0659
Andrea Carolina Mora López <acmoral@unal.edu.co>
2021-06-23 17:03:47 -0500

    the precision u.u
    so the precision showed weirdly, i finally added the np.round part in order for it to behave u.u

--- Codes/Percolation.py
+++ Codes/Percolation.py
@@ -8,7 +8,9 @@ Created on Mon Mar 29 10:16:10 2021
 from Graph import *
 import NetGraphics
 import matplotlib.pyplot as plt
-for i in range (0,20):# this was added so i can save images quickly
- j+=0.05;
- UndirectedGraph.PlotRectReal(j,1,sizex=36,sizey=5,scale=15.74)
+import numpy as np
+rang=np.linspace(0.05,1,20)
+rang=np.round(rang, 3)
+for i in rang:# this was added so i can save images quickly
+ UndirectedGraph.PlotRectReal(i,1,sizex=36,sizey=5,scale=15.74)
 #
