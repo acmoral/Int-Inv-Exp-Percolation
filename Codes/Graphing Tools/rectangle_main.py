f1e844fe6c977c532acb084e4d915b83311b3dc1
Andrea Mora <acmoral@unal.edu.co>
2022-01-19 20:59:11 -0500

    made some changes to visual aspects

--- Codes/Graphing Tools/rectangle_main.py
+++ Codes/Graphing Tools/rectangle_main.py
@@ -14,11 +14,12 @@ import pandas as pd
 I=0.5*0.003*0.003*0.003/12#The second moment of inertia of the material, respect to theZ axis
 so=0.30#The initial lenght of the material
 soc=so*so*so#cube it
-cycol = cycle(('darkgoldenrod','navy','purple','grey','teal','black','darkred'))
-marker = cycle(('s', '+', 'd', 'o', '*')) 
+cycol = cycle(('black','black'))
+cyful= cycle(('none','full'))
+marker = cycle(('s', 'o', 'v')) 
 fig, (ax1)=plt.subplots(1,1,figsize=(16,8))
-path=r'C:\Users\Carolina\OneDrive\Escritorio\Int inv Exp\videos y fotos medidas\Rectangular\excel'
-probs=[0,0.052,0.105,0.158,0.213,0.270,0.334]
+path=r'C:\Users\acmor\Desktop\excel'
+probs=[0.000,0.052,0.105,0.158,0.213,0.270,0.334]
 columns=['pendiente','intercepto','R','BSE','last','lastLog','lastLogerr','E','logE','Eerror','logEerror','p_err']
 df=showcase.init_tab(probs,columns)
 stops=[24,18,9,8,8,20,12]
@@ -34,16 +35,18 @@ for i in range(7):
  p0=showcase()
  stop0=stops[i]
  X0,Y0,weight0,height0=p0.returns(os.path.join(path,'P='+names[i]+'_A=3mm_H=1mm.csv'),1,stop0,probs[i])
- ax1.errorbar(weight0,height0,marker=next(marker),ls=' ',ms=6,label='P='+str(probs[i]),c=next(cycol),markerfacecolor="None", markeredgewidth=2)
- ax1.plot(X0,Y0,color='black',ls='dashed')
+ ax1.errorbar(weight0,height0,marker=next(marker),ls=' ',ms=8,c=next(cycol),fillstyle=next(cyful), markeredgewidth=2)
+ print(weight0[len(weight0)-1]-0.4,height0[len(height0)-1]+0.005)
+ ax1.text(weight0[len(weight0)-1]-0.6,height0[len(height0)-1]+0.009,str(probs[i]),fontsize=20)
+ ax1.plot(X0,Y0,color='black')
+ ax1.set_ylim(0,0.31)
  p0.tab(stop0,probs[i],df)
 #
 #---------------------------legends,titles-------------------
 #-----------------------------------------------------------
-ax1.set_xlabel(r'peso $[N] \pm 9.8\times10^{-4}$',fontsize=30)
-ax1.set_ylabel(r'Desplazamiento en Y $[m] $',fontsize=30)
+ax1.set_xlabel(r'Peso $[N]$',fontsize=30)
+ax1.set_ylabel(r' Flexión en Y $[m] $',fontsize=30)
 ax1.tick_params(axis='both', labelsize=20)
-ax1.set_title(r'Ancho= 3mm, Hueco= 1mm',size=30)
 ax1.legend(prop={'size': 20})
 ax1.xaxis.offsetText.set_fontsize(20)
 #-----------------------------------------------------------------
